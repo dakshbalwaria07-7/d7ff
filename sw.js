4303fb807f6ac5388f531afb80b75f5b942f0b18
D7FF <dakshbalwaria07@gmail.com>
2026-09-26 09:40:18 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE = 'd7ff-v1';
+var URLS = ['/', '/index.html'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}));
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.map(function(k){
+          if(k !== CACHE) return caches.delete(k);
+        })
+      );
+    })
+  );
+});
